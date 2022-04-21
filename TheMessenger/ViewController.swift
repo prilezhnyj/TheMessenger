c1ee687f5f8be5898aad386ed538891a4b763693
Maksim Botalov <maksimbotalov@icloud.com>
2022-04-21 12:17:28 +0500

    Initial Commit

---
+++ TheMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMessenger
+//
+//  Created by Максим Боталов on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
